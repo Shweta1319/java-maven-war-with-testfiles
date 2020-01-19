c22e3b00c4d5af8b86e305ac91068a0c96dc10ba
Shweta1319 <47740471+Shweta1319@users.noreply.github.com>
2020-01-19 12:12:43 +0530

    Update Jenkinsfile.sh

--- Jenkinsfile.sh
+++ Jenkinsfile.sh
@@ -12,7 +12,7 @@ pipeline {
     stages {
             stage('Checkout SCM') {
                     steps {
-                        deleteDir()
+                        //deleteDir()
                         //checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/Shweta1319/devops-java-maven-code.git']]])
                     }
            }
