20fa4125c9d8327903eb650f8d1f4fefccdd0ad2
Shweta1319 <47740471+Shweta1319@users.noreply.github.com>
2020-01-19 12:11:05 +0530

    Create Jenkinsfile.sh

---
+++ Jenkinsfile.sh
@@ -0,0 +1,50 @@
+pipeline {
+
+    agent {
+      label "master"
+     }
+          
+    tools {
+       maven 'Maven' 
+       jdk 'Java'      
+    }
+    
+    stages {
+            stage('Checkout SCM') {
+                    steps {
+                        deleteDir()
+                        //checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/Shweta1319/devops-java-maven-code.git']]])
+                    }
+           }
+           
+            stage('Build') {
+                    steps {
+                        sh 'mvn install'
+                        jacoco()
+                    }
+           }
+           
+           stage('Test'){
+                    steps{
+                      sh 'mvn test'
+                    }
+                    post {
+                        always {
+                            junit 'target/surefire-reports/*.xml'
+                        }
+                    }
+             }
+             
+            stage('DeploytoNexus'){
+                    steps{
+                        sh 'mvn deploy'                     
+                    }
+             }
+             
+             stage('UploadToSonarQube') {
+                    steps {
+                        sh 'mvn sonar:sonar'
+                    }
+           }
+    }
+}
